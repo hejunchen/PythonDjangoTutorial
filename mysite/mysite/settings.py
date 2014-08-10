8e0c23279012fd86c6086323956ca55459175648
Hejun Chen <hejunchen@gmail.com>
2014-08-10 00:38:27 -0700

    removed the poll app, now practise another tutorial videos

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'polls',
+    'article',
+
 )
 
 MIDDLEWARE_CLASSES = (
@@ -81,5 +82,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
+STATIC_ROOT = ''
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    ('assets', os.path.join(BASE_DIR, 'static')),
+)
